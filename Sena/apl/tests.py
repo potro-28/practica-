74832e245a9c501835a483130e48b3f2693e0d21
Juan Esteban Avila Perez <juanavila131204@gmail.com>
2026-07-02 08:14:17 -0500

    Correccion test.py

--- Sena/apl/tests.py
+++ Sena/apl/tests.py
@@ -5,10 +5,10 @@ from apl.models import Categoria
 class PruebaBasica(TestCase):
     def test_verificar_entorno(self):
         "una prueba simple para validar que CI/CD funciona"
-        self.assertTrue(1+1,2)
+        self.assertEqual(1+1,2)
 
     def test_crear_categoria(self):
         "Prueba basica de base de datos en memoria"
-        Categoria.objects.create(nombre='frijol')
+        categoria= Categoria.objects.create(nombre='frijol')
         consulta= Categoria.objects.filter(nombre='frijol')
-        self.assertEqual(consulta.exists())
+        self.assertTrue(consulta.exists())
